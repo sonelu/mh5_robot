cdaca1232fee50e9160aeb4bf4dc5e29700cc8fe
sonelu <alex.sonea@gmail.com>
2020-09-21 18:58:19 +0100

    move updates

--- mh5_director/src/test_runner.py
+++ mh5_director/src/test_runner.py
@@ -3,6 +3,7 @@ from mh5_controller.srv import ChangeTorque
 
 import rospy
 import actionlib
+import time
 
 
 rospy.init_node('script_user')
@@ -15,22 +16,31 @@ rospy.loginfo(f'Result\n{result.results}')
 client = actionlib.SimpleActionClient('director', RunScriptAction)
 client.wait_for_server()
 
-rospy.loginfo('Executing stand...')
 _ = input('Press when ready')
+time.sleep(2.0)
+rospy.loginfo('Executing stand...')
+# _ = input('Press when ready')
 command = RunScriptGoal('stand.yml')
 client.send_goal(command)
 client.wait_for_result()
 rospy.loginfo(f'result\n{client.get_result()}')
 
+rospy.loginfo('Executing wave...')
+# _ = input('Press when ready')
+command = RunScriptGoal('wave.yml')
+client.send_goal(command)
+client.wait_for_result()
+rospy.loginfo(f'result\n{client.get_result()}')
+
 rospy.loginfo('Executing sit...')
-_ = input('Press when ready')
+# _ = input('Press when ready')
 command = RunScriptGoal('sit.yml')
 client.send_goal(command)
 client.wait_for_result()
 rospy.loginfo(f'result\n{client.get_result()}')
 
 rospy.loginfo('Setting torque off...')
-_ = input('Press when ready')
+# _ = input('Press when ready')
 torque_server = rospy.ServiceProxy('change_torque', ChangeTorque)
 result = torque_server(False, [], ['all'])
 rospy.loginfo(f'Result\n{result.results}')
