b98faf7595b4ba39b65489c1cbd1baf4560c16fc
sonelu <alex.sonea@gmail.com>
2021-03-11 20:38:29 +0000

    hardware_interface first draft

---
+++ mh5_hardware_control/include/mh5_hardware_control/mh5_dynamixel_interface.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <ros/ros.h>
+
+namespace mh5_hardware_interface
+{
+
+class MH5DynamixelInterface: public hardware_interface::RobotHW
+{
+public:
+    MH5DynamixelInterface();
+    ~MH5DynamixelInterface();
+    bool init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh);
+    void read(const ros::Time& time, const ros::Duration& period);
+    void write(const ros::Time& time, const ros::Duration& period);
+
+protected:
+    ros::NodeHandle nh_;
+    
+    //interfaces
+    hardware_interface::JointStateInterface joint_state_interface;
+    hardware_interface::EffortJointInterface effort_joint_interface;
+
+    int num_joints;
+    std::vector<std::string> joint_name;
+
+    //actual states
+    std::vector<double> joint_position_state;
+    std::vector<double> joint_velocity_state;
+    std::vector<double> joint_effort_state;
+
+    //given setpoints
+    std::vector<double> joint_effort_command;
+
+};
+}
